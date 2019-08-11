3de591add14df711208bc7144b2b18c160a6de65
Nick Joyce <brawlyrox@gmail.com>
2019-08-11 14:13:32 +0100

    Updates

---
+++ precache-manifest.4d319f18036760aeb1024bfe07389628.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "144b220e330cd54531258fdd0f7fd824",
+    "url": "/live-doc-demo/index.html"
+  },
+  {
+    "revision": "18ec0e46ed2f98a171b4",
+    "url": "/live-doc-demo/static/css/2.93e9ecf4.chunk.css"
+  },
+  {
+    "revision": "a0425a8877d7d519fc66",
+    "url": "/live-doc-demo/static/css/main.6ffb3f2b.chunk.css"
+  },
+  {
+    "revision": "18ec0e46ed2f98a171b4",
+    "url": "/live-doc-demo/static/js/2.5ea288ae.chunk.js"
+  },
+  {
+    "revision": "a0425a8877d7d519fc66",
+    "url": "/live-doc-demo/static/js/main.a44e7d1e.chunk.js"
+  },
+  {
+    "revision": "7fd2030b27d55c2483b2",
+    "url": "/live-doc-demo/static/js/runtime~main.693dc1b3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/live-doc-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
